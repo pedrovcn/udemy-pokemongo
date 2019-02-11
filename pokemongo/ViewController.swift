8ecb3377689978b95d34c44a2a92415eaabb3655
Pedro Nascimento <p.nascimento@cinq.com.br>
2019-02-11 14:59:28 -0200

    Initial Commit

---
+++ pokemongo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokemongo
+//
+//  Created by Pedro Vinicius Cuêlho do Nascimento on 11/02/2019.
+//  Copyright © 2019 Pedro Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
